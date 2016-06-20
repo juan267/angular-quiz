c539a41e50e4a2382d413ca9a1cd16d0374d08a5
juan267 <j.gomez267@gmail.com>
2016-06-19 22:25:12 -0500

    added change betwenn quetions and feedback states

--- js/quiz.js
+++ js/quiz.js
@@ -41,6 +41,10 @@
       return $scope.myQuestions[qIndex].correctAnswer === aIndex
     }
 
+    $scope.selectContinue = function(){
+      return $scope.activeQuestion += 1
+    }
+
  }])
 
 })();
